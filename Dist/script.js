6ef71604300d7809e1cab49676a3a650b3677f7b
Sourav nandi <118915260+sourav0009@users.noreply.github.com>
2024-10-24 19:55:32 +0530

    last

--- Dist/script.js
+++ Dist/script.js
@@ -3,6 +3,7 @@ const apiKey = "4330d982e481526bae65e99c8ad2b5bf";
 
         const searchBox = document.querySelector("#search input"); // Use #search instead of .search
         const searchBtn = document.querySelector("#search button"); // Use #search instead of .search
+        const weatherIcon = document.querySelector("#weather-icon")
 
         async function checkWeather(city) {
             const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
@@ -12,6 +13,20 @@ const apiKey = "4330d982e481526bae65e99c8ad2b5bf";
             document.getElementById("temp").innerHTML = Math.round(data.main.temp) + "°C";
             document.getElementById("Humidity").innerHTML = data.main.humidity + "%";
             document.getElementById("wind").innerHTML = data.wind.speed + " km/h";
+
+            if(data.weather[0].main == "Clouds"){
+                weatherIcon.src = "./images/clouds.png";
+            } else if(data.weather[0].main == "Clear"){
+                weatherIcon.src = "./images/clear.png";
+            }else if(data.weather[0].main == "Rain"){
+                weatherIcon.src = "./images/rain.png";
+            }else if(data.weather[0].main == "Drizzle"){
+                weatherIcon.src = "./images/drizzle.png";
+            }else if(data.weather[0].main == "Mist"){
+                weatherIcon.src = "./images/rain.png";
+            }
+
+
         }
 
         searchBtn.addEventListener("click", () => {
