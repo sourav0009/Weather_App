a0eaae4d2180fb8d53e4b482f2ec03ce682c4c71
Sourav nandi <118915260+sourav0009@users.noreply.github.com>
2024-10-24 18:02:58 +0530

    1sr responciveness

--- Dist/script.js
+++ Dist/script.js
@@ -1,14 +1,20 @@
 const apiKey = "4330d982e481526bae65e99c8ad2b5bf";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore";
+        const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-async function chakeWeather() {
-    const responce = await fetch(apiUrl + `&appid=${apiKey}`);
-    var data = await responce.json();
-    console.log(data)
-    document.getElementById("city").innerHTML = data.name;
-    document.getElementById("temp").innerHTML = Math.round(data.main.temp) + "°C";
-    document.getElementById("Humidity").innerHTML = data.main.humidity + "%";
-    document.getElementById("wind").innerHTML = data.wind.speed + "km/h";
+        const searchBox = document.querySelector("#search input"); // Use #search instead of .search
+        const searchBtn = document.querySelector("#search button"); // Use #search instead of .search
 
-}
-chakeWeather();
+        async function checkWeather(city) {
+            const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+            var data = await response.json();
+            console.log(data);
+            document.getElementById("city").innerHTML = data.name;
+            document.getElementById("temp").innerHTML = Math.round(data.main.temp) + "°C";
+            document.getElementById("Humidity").innerHTML = data.main.humidity + "%";
+            document.getElementById("wind").innerHTML = data.wind.speed + " km/h";
+        }
+
+        searchBtn.addEventListener("click", () => {
+            checkWeather(searchBox.value);
+        });
+
